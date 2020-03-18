1bbb6051a658be4c381068ed465916a8a7044510
unknown <msubhashree2000@gmail.com>
2020-03-18 19:16:48 +0530

    done

---
+++ src/com/tgt/igniteplus/Main.java
@@ -0,0 +1,22 @@
+package com.tgt.igniteplus;
+import java.util.Scanner;
+//question-one
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num=in.nextInt();
+        int i,j;
+        for(i=1;i<=num;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
